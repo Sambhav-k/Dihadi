64d66f32586149eb89f5f31d8fb60ef89d91666f
Your Name <you@example.com>
2024-05-05 23:07:35 +0530

    Initial commit

---
+++ src/components/Application/MyApplications.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const MyApplication=() =>{
+    return(
+        <div>
+            
+        </div>
+    )
+        
+    
+}
+
+export default MyApplication
---
+++ src/components/Application/ResumeModal.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const ResumeModal=() =>{
+    return(
+        <div>
+            
+        </div>
+    )
+        
+    
+}
+
+export default ResumeModal
